ab5090045f08d94d487a7fd150734480e4b387e0
Athira K S <athiraks@Athiras-MacBook-Air.local>
2023-05-11 13:07:20 +0530

    First section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultCardComponent } from './result-card/result-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultCardComponent
   ],
   imports: [
     BrowserModule,
